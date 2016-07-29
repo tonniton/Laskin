57a286e613051530e798ccb760bd9082013d1f86
tonniton <viljami.paloposki@gmail.com>
2016-07-29 11:48:57 +0300

    testi

--- Drinkkikone/src/main/java/drinkkikone/osat/Ainesosa.java
+++ Drinkkikone/src/main/java/drinkkikone/osat/Ainesosa.java
@@ -11,4 +11,24 @@ package drinkkikone.osat;
  */
 public class Ainesosa {
     
+    private String nimi;
+    private String maara;
+
+    public Ainesosa(String nimi) {
+        this.nimi = nimi;
+        this.maara = "";
+    }
+
+    public void setMaara(String maara) {
+        this.maara = maara;
+    }
+
+    @Override
+    public String toString() {
+        return nimi + " " + maara;
+    }
+    
+    
+    
+    
 }
--- Drinkkikone/src/main/java/drinkkikone/osat/Drinkki.java
+++ Drinkkikone/src/main/java/drinkkikone/osat/Drinkki.java
@@ -5,10 +5,23 @@
  */
 package drinkkikone.osat;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Viljami
  */
 public class Drinkki {
+    
+    private String nimi;
+    private ArrayList<Ainesosa> aineet;
+    private String resepti;
+
+    public Drinkki(String nimi, String resepti) {
+        this.nimi = nimi;
+        this.aineet = new ArrayList();
+        this.resepti = resepti;
+    }
+    
     
 }

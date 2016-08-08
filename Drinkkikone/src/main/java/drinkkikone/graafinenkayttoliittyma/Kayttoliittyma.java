8fd211e0842ff5dafeedd3b8d094003a50b8e937
tonniton <viljami.paloposki@gmail.com>
2016-08-08 11:39:27 +0300

    testi

--- Drinkkikone/src/main/java/drinkkikone/graafinenkayttoliittyma/Kayttoliittyma.java
+++ Drinkkikone/src/main/java/drinkkikone/graafinenkayttoliittyma/Kayttoliittyma.java
@@ -11,6 +11,7 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.util.Vector;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -40,42 +41,34 @@ public class Kayttoliittyma implements Runnable {
     }
 
     private void luoKomponentit(Container container) {
-        container.add(new JButton("Pohjoinen (North)"), BorderLayout.NORTH);
-
-        JTextField vasen = new JTextField("0");
-        vasen.setEnabled(false);
-        JTextField oikea = new JTextField("");
-        String	listData[] =
-		{
-			"Item 1",
-			"Item 2",
-			"Item 3",
-			"Item 4"
-		};
+        
+        Vector listData = new Vector();
         JList listbox = new JList(listData);
-//        JPanel valikko = luovalikko(vasen, oikea);
-//        container.add(valikko, BorderLayout.SOUTH);
+//        listbox.addListSelectionListener(this);
+        JTextField syote = new JTextField();
         
         JPanel menu = new JPanel(new GridLayout(1, 3));
-        menu.add(vasen);
-        menu.add(oikea);
         menu.add(listbox);
+        menu.add(syote);
         container.add(menu);
+        
+        JPanel valikko = luovalikko(listbox, syote, listData);
+        container.add(valikko, BorderLayout.SOUTH);
     }
     
-    private JPanel luovalikko(JTextField vasen, JTextField oikea) {
+    private JPanel luovalikko(JList vasen, JTextField oikea, Vector listData) {
         JPanel panel = new JPanel(new GridLayout(1, 3));
         JButton plus = new JButton("+");
         JButton miinus = new JButton("-");
         JButton nollaa = new JButton("Z");
-        Kuuntelija kuuntelija = new Kuuntelija(oikea, vasen, plus, miinus, nollaa);
+        Kuuntelija kuuntelija = new Kuuntelija(vasen, oikea, plus, miinus, nollaa, listData);
         plus.addActionListener(kuuntelija);
-        miinus.addActionListener(kuuntelija);
-        nollaa.addActionListener(kuuntelija);
+//        miinus.addActionListener(kuuntelija);
+//        nollaa.addActionListener(kuuntelija);
         panel.add(plus);
-        panel.add(miinus);
-        nollaa.setEnabled(false);
-        panel.add(nollaa);
+//        panel.add(miinus);
+//        nollaa.setEnabled(false);
+//        panel.add(nollaa);
         return panel;
     }
 
@@ -83,3 +76,41 @@ public class Kayttoliittyma implements Runnable {
         return frame;
     }
 }
+
+//    private void luoKomponentit(Container container) {
+////        container.add(new JButton("Pohjoinen (North)"), BorderLayout.NORTH);
+//        
+//        Vector listData = new Vector();
+//        JList listbox = new JList(listData);
+////        listbox.addListSelectionListener(this);
+//
+//        JTextField vasen = new JTextField("0");
+//        vasen.setEnabled(false);
+//        JTextField oikea = new JTextField("");
+//////        String	listData[] = {"Item 1"};
+//////        JList listbox = new JList(listData);
+////        JPanel valikko = luovalikko(vasen, oikea);
+////        container.add(valikko, BorderLayout.SOUTH);
+//        
+//        JPanel menu = new JPanel(new GridLayout(1, 3));
+//        menu.add(vasen);
+//        menu.add(oikea);
+//        menu.add(listbox);
+//        container.add(menu);
+//    }
+//    
+//    private JPanel luovalikko(JTextField vasen, JTextField oikea) {
+//        JPanel panel = new JPanel(new GridLayout(1, 3));
+//        JButton plus = new JButton("+");
+//        JButton miinus = new JButton("-");
+//        JButton nollaa = new JButton("Z");
+//        Kuuntelija kuuntelija = new Kuuntelija(oikea, vasen, plus, miinus, nollaa);
+//        plus.addActionListener(kuuntelija);
+//        miinus.addActionListener(kuuntelija);
+//        nollaa.addActionListener(kuuntelija);
+//        panel.add(plus);
+//        panel.add(miinus);
+//        nollaa.setEnabled(false);
+//        panel.add(nollaa);
+//        return panel;
+//    }
--- Drinkkikone/src/main/java/drinkkikone/graafinenkayttoliittyma/Kuuntelija.java
+++ Drinkkikone/src/main/java/drinkkikone/graafinenkayttoliittyma/Kuuntelija.java
@@ -7,62 +7,40 @@ package drinkkikone.graafinenkayttoliittyma;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Vector;
 import javax.swing.JButton;
+import javax.swing.JList;
 import javax.swing.JTextField;
 
 public class Kuuntelija implements ActionListener {
-    private JTextField kentta1;
-    private JTextField kentta2;
+    private JList osat;
+    private JTextField lisaa;
     private JButton plus;
     private JButton miinus;
     private JButton nollaa;
+    private Vector listData;
 
-    public Kuuntelija(JTextField kentta1, JTextField kentta2, JButton plus, JButton miinus, JButton nollaa) {
-        this.kentta1 = kentta1;
-        this.kentta2 = kentta2;
-        this.plus = plus;
+    public Kuuntelija(JList kentta1, JTextField kentta2, JButton lisaa, JButton miinus, JButton nollaa, Vector listData) {
+        this.osat = kentta1;
+        this.lisaa = kentta2;
+        this.plus = lisaa;
         this.miinus = miinus;
         this.nollaa = nollaa;
+        this.listData  = listData;
     }
     
     @Override
     public void actionPerformed(ActionEvent ae) {
-        String luku2 = kentta2.getText();
-//        int luku2 = muutaluvuksi(kentta2);
         try {
-            String luku1 = kentta1.getText();
-//            int luku1 = muutaluvuksi(kentta1);
-        
+            String lisattava = lisaa.getText();
             if (ae.getSource() == plus) {
-//                luku2 += luku1;
-                kentta2.setText(kentta2.getText() + "\n" + luku1);
-                kentta1.setText("");
-            } else if (ae.getSource() == miinus) {
-//                luku2 -= luku1;
-                kentta2.setText("" + luku2);
-                kentta1.setText("");
-            } else {
-                kentta2.setText("0");
-                kentta1.setText("");
-            }
-
-            if (kentta2.getText().equals("0")) {
-                nollaa.setEnabled(false);
-            } else {
-                nollaa.setEnabled(true);
+                lisaa.setText("");
+                listData.addElement(lisattava);
+                osat.setListData(listData);
             }
         } catch (Exception e) {
-            kentta1.setText("");
+
         }
         
-    }
-    
-    public int muutaluvuksi(JTextField kentta) {
-        String teksti = kentta.getText();
-        if (teksti.isEmpty()) {
-            return 0;
-        }
-        int luku = Integer.parseInt(teksti);
-        return luku;
     }
 }

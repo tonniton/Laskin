1593026dc2a34e6e03df213ab4ed7551fbdec0f8
tonniton <viljami.paloposki@gmail.com>
2016-08-21 14:25:59 +0300

    uusi gui

--- Drinkkikone/src/main/java/drinkkikone/graafinenkayttoliittyma/Drinkinkuuntelija.java
+++ Drinkkikone/src/main/java/drinkkikone/graafinenkayttoliittyma/Drinkinkuuntelija.java
@@ -14,7 +14,11 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JViewport;
 
 /**
  *
@@ -27,15 +31,21 @@ public class Drinkinkuuntelija implements ActionListener {
     private Container container;
     private JButton lisaaNappi;
     private Kirjanpito kirjanpito;
+    private JPanel paneeli;
+    private JPanel paneeli2;
 
     /**
      *
      * @param container käytössä oleva container olio.
+     * @param paneeli
+     * @param paneeli2
      */
-    public Drinkinkuuntelija(Container container) {
+    public Drinkinkuuntelija(Container container, JPanel paneeli,  JPanel paneeli2) {
         this.container = container;
-        lisaaNappi = (JButton) container.getComponent(2);
+        lisaaNappi = (JButton) paneeli.getComponent(36);
         this.kirjanpito = Kirjanpito.getInstance();
+        this.paneeli = paneeli;
+        this.paneeli2 = paneeli2;
     }
     
     
@@ -52,16 +62,18 @@ public class Drinkinkuuntelija implements ActionListener {
      * 
      */
     public void lisaaDrinkki() {
-        String nimi = ((JTextField) container.getComponent(1)).getText();
-        String resepti = ((JTextField) container.getComponent(4)).getText();
+        String nimi = ((JTextField) paneeli.getComponent(1)).getText();
+        JTextArea ohje = (JTextArea) ((JViewport) (((JScrollPane) paneeli2.getComponent(1)).getViewport())).getView();
+        String resepti = ohje.getText();
+//        String resepti = ((JTextArea) ((JScrollPane) paneeli2.getComponent(1)).getComponent(0)).getText();
         if (nimi.isEmpty() || resepti.isEmpty()) {
             return;
         }
         Drinkki drinkki = new Drinkki(nimi, resepti);
-        for (int i = 9; i < 39; i += 3) {
-            String osanimi = ((JTextField) container.getComponent(i)).getText();
-            String maara = ((JTextField) container.getComponent(i + 1)).getText();
-            boolean tarkea = ((JCheckBox) container.getComponent(i + 2)).isSelected();
+        for (int i = 6; i < 36; i += 3) {
+            String osanimi = ((JTextField) paneeli.getComponent(i)).getText();
+            String maara = ((JTextField) paneeli.getComponent(i + 1)).getText();
+            boolean tarkea = ((JCheckBox) paneeli.getComponent(i + 2)).isSelected();
             if (osanimi.isEmpty()) {
                 break;
             }
@@ -75,12 +87,13 @@ public class Drinkinkuuntelija implements ActionListener {
     }
 
     private void tyhjennaTekstit() {
-        ((JTextField) container.getComponent(1)).setText("");
-        ((JTextField) container.getComponent(4)).setText("");
-        for (int i = 9; i < 39; i += 3) {
-            ((JTextField) container.getComponent(i)).setText("");
-            ((JTextField) container.getComponent(i + 1)).setText("");
-            ((JCheckBox) container.getComponent(i + 2)).setSelected(false);
+        ((JTextField) paneeli.getComponent(1)).setText("");
+        JTextArea ohje = (JTextArea) ((JViewport) (((JScrollPane) paneeli2.getComponent(1)).getViewport())).getView();
+        ohje.setText("");
+        for (int i = 6; i < 36; i += 3) {
+            ((JTextField) paneeli.getComponent(i)).setText("");
+            ((JTextField) paneeli.getComponent(i + 1)).setText("");
+            ((JCheckBox) paneeli.getComponent(i + 2)).setSelected(false);
         }
     }
 }
--- Drinkkikone/src/main/java/drinkkikone/graafinenkayttoliittyma/Drinkinlisaaja.java
+++ Drinkkikone/src/main/java/drinkkikone/graafinenkayttoliittyma/Drinkinlisaaja.java
@@ -5,14 +5,21 @@
  */
 package drinkkikone.graafinenkayttoliittyma;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
@@ -48,33 +55,51 @@ public class Drinkinlisaaja implements Runnable {
 
     private void luoKomponentit(Container container) {
         GridLayout layout = new GridLayout(13, 3);
-        container.setLayout(layout);
-
+        JPanel paneeli = new JPanel();
+        paneeli.setLayout(layout);
+        container.add(paneeli, BorderLayout.CENTER);
+        JPanel paneeli2 = new JPanel();
+        paneeli2.setLayout(new BorderLayout());
+        container.add(paneeli2, BorderLayout.EAST);
+//        container.setLayout(layout);
+        
         JLabel drinkinNimi = new JLabel("Drinkin nimi: ");
         JTextField nimiKentta = new JTextField();
         JLabel resepti = new JLabel("Resepti: ");
         JTextField reseptiKentta = new JTextField();
         JButton lisaaNappi = new JButton("Lisää Drinkki!");
 
-        container.add(drinkinNimi);
-        container.add(nimiKentta);
-        container.add(lisaaNappi);
-        container.add(resepti);
-        container.add(reseptiKentta);
-        container.add(new JLabel(""));
+        JTextArea textArea = new JTextArea(10, 10);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        JScrollPane scrollPane = new JScrollPane(textArea);
         
+        paneeli.add(drinkinNimi);
+        paneeli.add(nimiKentta);
+        paneeli.add(new JLabel(""));
+//        paneeli.add(lisaaNappi);
+        paneeli2.add(resepti, BorderLayout.NORTH);
+        paneeli2.add(scrollPane, BorderLayout.CENTER);
+//        container.add(reseptiKentta);
+//        container.add(scrollPane);
+//        container.add(new JLabel(""));
         
-        container.add(new JLabel("Ainesosan nimi: "));
-        container.add(new JLabel("Ainesosan määrä: "));
-        container.add(new JLabel("Välttämätön: "));
+        paneeli.add(new JLabel("Ainesosan nimi: "));
+        paneeli.add(new JLabel("Ainesosan määrä: "));
+        paneeli.add(new JLabel("Välttämätön: "));
         for (int i = 0; i < 10; i++) {
-            container.add(new JTextField());
-            container.add(new JTextField());
-            container.add(new JCheckBox());
+            paneeli.add(new JTextField());
+            paneeli.add(new JTextField());
+            paneeli.add(new JCheckBox());
         }
+        paneeli.add(lisaaNappi);
         
-        Drinkinkuuntelija kuuntelija = new Drinkinkuuntelija(container);
+        Drinkinkuuntelija kuuntelija = new Drinkinkuuntelija(container, paneeli, paneeli2);
         lisaaNappi.addActionListener(kuuntelija);
+    }
+    
+    private void luoReseptiOsa() {
+        
     }
 
     /**

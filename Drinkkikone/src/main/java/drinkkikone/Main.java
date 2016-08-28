c2c64c4caf16e8565420406f927b4aaa5d4f8a9b
tonniton <viljami.paloposki@gmail.com>
2016-08-28 19:24:44 +0300

    .jar

--- Drinkkikone/src/main/java/drinkkikone/Main.java
+++ Drinkkikone/src/main/java/drinkkikone/Main.java
@@ -1,11 +1,24 @@
 package drinkkikone;
 
 import drinkkikone.graafinenkayttoliittyma.Kayttoliittyma;
+import java.io.File;
+import java.net.URISyntaxException;
 import javax.swing.SwingUtilities;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException {
+//        String ohjelmanSuoritusPolku = Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
+//        System.out.println("Suorituspolku: " + ohjelmanSuoritusPolku);
+//        File file = null;
+//        file = new File(ohjelmanSuoritusPolku + "tietokannat/tietokanta.txt");
+//        System.out.println("Kansisosa");
+//        if (file.exists()) {
+//            System.out.println("tiedosto löytyi!");
+//        } else {
+//            System.out.println("tiedosto ei löytynyt!");
+//        }
+        System.out.println(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
         Kayttoliittyma kayttoliittyma = new Kayttoliittyma();
         SwingUtilities.invokeLater(kayttoliittyma);
     }
--- Drinkkikone/src/main/java/drinkkikone/graafinenkayttoliittyma/IkkunaKuuntelija.java
+++ Drinkkikone/src/main/java/drinkkikone/graafinenkayttoliittyma/IkkunaKuuntelija.java
@@ -5,6 +5,7 @@
  */
 package drinkkikone.graafinenkayttoliittyma;
 
+import drinkkikone.Main;
 import drinkkikone.osat.Kirjanpito;
 import drinkkikone.tietokanta.Tiedostonlukija;
 import drinkkikone.tietokanta.Tiedostoonkirjoittaja;
@@ -12,6 +13,9 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JList;
 
 /**
@@ -33,15 +37,22 @@ public class IkkunaKuuntelija implements WindowListener {
     public IkkunaKuuntelija(JList lista) {
         kirjanpito = Kirjanpito.getInstance();
         this.lista = lista;
-        polku = "src/main/resources/tietokannat/tietokanta.txt";
         try {
-            lukija = new Tiedostonlukija(kirjanpito, polku);
-        } catch (FileNotFoundException ex) {
+            //        polku = "src/main/resources/tietokannat/tietokanta.txt";
+            polku = Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath() + "tietokannat/tietokanta.txt";
+        } catch (URISyntaxException ex) {
+            System.out.println("polku");
+            Logger.getLogger(IkkunaKuuntelija.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
     @Override
     public void windowOpened(WindowEvent e) {
+        try {
+            lukija = new Tiedostonlukija(kirjanpito, polku);
+        } catch (FileNotFoundException ex) {
+            System.out.println("open");
+        }
         lukija.lueTiedosto();
     }
 
@@ -50,10 +61,12 @@ public class IkkunaKuuntelija implements WindowListener {
         try {
             kirjoittaja = new Tiedostoonkirjoittaja(false, polku);
         } catch (IOException ex) {
+            System.out.println("close");
         }
         try {
             kirjoittaja.lisaaTiedostoon(kirjanpito.getDrinkit());
         } catch (IOException ex) {
+            System.out.println("close2");
         }
     }
 
